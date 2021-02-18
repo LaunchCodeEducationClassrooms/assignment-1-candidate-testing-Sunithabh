8fd4408df8420380a57a25a532a693ffe2a20d7e
sunithaprasad <sunithaprasad.c@gmail.com>
2021-02-18 02:36:00 +0000

    Modified "questions" and "grade" part

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,12 +8,12 @@ let candidateName = "";
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
-let questions = ["Who was the first American woman in space? ","True or false: 5000 meters == 5 kilometers? ","(5 + 3)/2 * 10 = ? ","Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ","What is the minimum crew size for the ISS? "];
-let correctAnswers = ["Sally Ride","True","40","Trajectory","3"];
+let questions = ["Who was the first American woman in space? " ,"True or false: 5 kilometer == 5000 meters? " ,"(5 + 3)/2 * 10 = ? " ,"Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? " ,"What is the minimum crew size for the ISS? "];
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
 let candidateAnswers= [];
-let result ="";
+let result = "";
 let totalGrade = 0;
-
+ 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
@@ -26,11 +26,6 @@ function askQuestion() {
     candidateAnswers[i] = input.question(questions[i]);
     console.log(`Your Answer: ${candidateAnswers[i]}`);
     console.log(`Correct Answer: ${correctAnswers[i]}`);
-
-   if (candidateAnswers[i] == correctAnswers[i].toLowerCase()) {
-     candidateAnswers[i].toLowerCase();
-     totalGrade++;
-    }
   } 
 
 }
@@ -40,11 +35,16 @@ function gradeQuiz(candidateAnswers) {
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 
 
+ for (i=0;i<correctAnswers.length;i++) {
+   if (candidateAnswers[i].toLowerCase() == correctAnswers[i].toLowerCase()) {
+    totalGrade++;
+   } 
+  }
   let grade;
-  grade = totalGrade/(questions.length) * 100;
+  grade = totalGrade / questions.length * 100;
   if (grade >= 80 ) {
     result = "passed";
-  } else {
+   } else {
     result = "FAILED";
   } 
   console.log(`>>> Overall Grade: ${grade}% (${totalGrade} of ${questions.length} responses correct) <<<`);
